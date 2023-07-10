8ab00138375c938235cbe04ad387b2f5c8662f89
dev-seongjoo <dev.seongjoo@gmail.com>
2023-07-10 13:52:47 +0900

    feat: 선수별 출전기록 선수별 출전 기록 UI 완료

--- src/components/Match/MatchRecord/Participation/Participation.js
+++ src/components/Match/MatchRecord/Participation/Participation.js
@@ -1,5 +1,195 @@
+import {
+  AssistTd,
+  AssistTh,
+  Container,
+  GoalTd,
+  GoalTh,
+  LpTd,
+  LpTh,
+  MatchNumTd,
+  MatchNumTh,
+  NameTd,
+  NameTh,
+  OgTd,
+  OgTh,
+  Option,
+  PositionTd,
+  PositionTh,
+  ProfileGroup,
+  ProfileImg,
+  ProfileName,
+  Select,
+  SubInTd,
+  SubInTh,
+  SubOutTd,
+  SubOutTh,
+  Table,
+  TbodydTr,
+  TheadTr,
+  Title,
+} from "./styled";
+
+import ProfileBasicImg from "../../../../assets/basic-profile.jpg";
+
 const Participation = () => {
-  return <h1>Participation</h1>;
+  const dummyData = [
+    {
+      name: "홍길동",
+      position: "GK",
+      matchNum: 10,
+      subIn: 2,
+      subOut: 1,
+      goal: 0,
+      assist: 0,
+      lp: 8,
+      og: 1,
+    },
+    {
+      name: "김철수",
+      position: "CB",
+      matchNum: 8,
+      subIn: 3,
+      subOut: 4,
+      goal: 1,
+      assist: 2,
+      lp: 0,
+      og: 0,
+    },
+    {
+      name: "박영희",
+      position: "LM",
+      matchNum: 9,
+      subIn: 1,
+      subOut: 2,
+      goal: 2,
+      assist: 3,
+      lp: 0,
+      og: 0,
+    },
+    {
+      name: "이영호",
+      position: "RM",
+      matchNum: 7,
+      subIn: 2,
+      subOut: 3,
+      goal: 0,
+      assist: 1,
+      lp: 0,
+      og: 0,
+    },
+    {
+      name: "김민지",
+      position: "ST",
+      matchNum: 6,
+      subIn: 4,
+      subOut: 2,
+      goal: 4,
+      assist: 1,
+      lp: 0,
+      og: 0,
+    },
+    {
+      name: "정기철",
+      position: "CDM",
+      matchNum: 5,
+      subIn: 3,
+      subOut: 1,
+      goal: 0,
+      assist: 0,
+      lp: 0,
+      og: 0,
+    },
+    {
+      name: "이승호",
+      position: "LB",
+      matchNum: 8,
+      subIn: 0,
+      subOut: 2,
+      goal: 0,
+      assist: 0,
+      lp: 0,
+      og: 0,
+    },
+    {
+      name: "김지연",
+      position: "RW",
+      matchNum: 9,
+      subIn: 1,
+      subOut: 0,
+      goal: 1,
+      assist: 3,
+      lp: 0,
+      og: 0,
+    },
+    {
+      name: "박성준",
+      position: "CAM",
+      matchNum: 7,
+      subIn: 1,
+      subOut: 3,
+      goal: 3,
+      assist: 0,
+      lp: 0,
+      og: 0,
+    },
+    {
+      name: "이나라",
+      position: "LW",
+      matchNum: 6,
+      subIn: 2,
+      subOut: 1,
+      goal: 1,
+      assist: 1,
+      lp: 0,
+      og: 0,
+    },
+  ];
+
+  return (
+    <>
+      <Title>선수별 출전기록</Title>
+      <Container>
+        <Select>
+          <Option>2023</Option>
+        </Select>
+        <Table>
+          <thead>
+            <TheadTr>
+              <NameTh>이름</NameTh>
+              <PositionTh>포지션</PositionTh>
+              <MatchNumTh>출전</MatchNumTh>
+              <SubInTh>교체 IN</SubInTh>
+              <SubOutTh>교체 OUT</SubOutTh>
+              <GoalTh>득점</GoalTh>
+              <AssistTh>도움</AssistTh>
+              <LpTh>실점</LpTh>
+              <OgTh>자책</OgTh>
+            </TheadTr>
+          </thead>
+          <tbody>
+            {dummyData.map((data) => (
+              <TbodydTr>
+                <NameTd>
+                  <ProfileGroup>
+                    <ProfileImg src={ProfileBasicImg} />
+                    <ProfileName>{data.name}</ProfileName>
+                  </ProfileGroup>
+                </NameTd>
+                <PositionTd>{data.position}</PositionTd>
+                <MatchNumTd>{data.matchNum}</MatchNumTd>
+                <SubInTd>{data.subIn}</SubInTd>
+                <SubOutTd>{data.subOut}</SubOutTd>
+                <GoalTd>{data.goal}</GoalTd>
+                <AssistTd>{data.assist}</AssistTd>
+                <LpTd>{data.lp}</LpTd>
+                <OgTd>{data.og}</OgTd>
+              </TbodydTr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+    </>
+  );
 };
 
 export default Participation;
---
+++ src/components/Match/MatchRecord/Participation/styled.js
@@ -0,0 +1,197 @@
+import styled from "styled-components";
+
+export const Title = styled.div`
+  margin: 60px 0px;
+  font-family: "Noto Sans Display", sans-serif;
+  font-size: 2rem;
+  font-weight: 500;
+  text-align: center;
+`;
+
+export const Container = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+`;
+
+export const Select = styled.select`
+  padding: 5px;
+  border: none;
+  font-size: 0.8rem;
+  color: rgba(0, 0, 0, 0.5);
+  margin-bottom: 5px;
+`;
+
+export const Option = styled.option``;
+
+export const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+`;
+
+export const TheadTr = styled.tr`
+  height: 80px;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  background-color: #f5f5f5;
+`;
+
+export const NameTh = styled.th`
+  width: 20%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const PositionTh = styled.th`
+  width: 10%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const MatchNumTh = styled.th`
+  width: 10%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const SubInTh = styled.th`
+  width: 10%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const SubOutTh = styled.th`
+  width: 10%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const GoalTh = styled.th`
+  width: 7.69%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const AssistTh = styled.th`
+  width: 7.69%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const LpTh = styled.th`
+  width: 7.69%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+export const OgTh = styled.th`
+  width: 7.69%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const TbodydTr = styled.tr`
+  height: 80px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+`;
+
+export const NameTd = styled.th`
+  width: 20%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const ProfileGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ProfileImg = styled.img`
+  width: 50px;
+  height: 50px;
+  margin-right: 20px;
+`;
+
+export const ProfileName = styled.span``;
+
+export const PositionTd = styled.th`
+  width: 10%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const MatchNumTd = styled.th`
+  width: 10%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const SubInTd = styled.th`
+  width: 10%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const SubOutTd = styled.th`
+  width: 10%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const GoalTd = styled.th`
+  width: 7.69%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const AssistTd = styled.th`
+  width: 7.69%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const LpTd = styled.th`
+  width: 7.69%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
+export const OgTd = styled.th`
+  width: 7.69%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: middle;
+`;
